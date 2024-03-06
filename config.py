1079ad93b8b452b6a8cd53355bcd0e1f0d0f9306
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-03-06 15:57:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 # config.py
 
 import os
+import json
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-DATABASE_URL = os.environ.get("DATABASE_URL")
+def load_config():
+    # Check if running on Heroku
+    if 'DATABASE_URL' in os.environ:
+        # Running on Heroku, load config from environment variables
+        return {
+            'BOT_TOKEN': os.environ.get('BOT_TOKEN'),
+            'DATABASE_URL': os.environ.get('DATABASE_URL')
+        }
+    else:
+        # Not running on Heroku, load config from app.json
+        with open('app.json', 'r') as file:
+            config_data = json.load(file)
+        return {
+            'BOT_TOKEN': config_data['env']['BOT_TOKEN'],
+            'DATABASE_URL': config_data['env']['DATABASE_URL']
+        }
+
+config = load_config()
